5d657163469ff7b1c47ef71dfb5f81a33c8afdd4
Milad <miladxgh@outlook.com>
2021-07-27 12:40:17 +0430

    first commit

---
+++ script.js
@@ -0,0 +1,76 @@
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let ctx = canvas.getContext("2d");
+
+class Pixel {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.opacity = 1;
+    this.hue = 255;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = `hsla(${this.hue}, 100%, 40%, ${this.opacity})`;
+    ctx.fill();
+    ctx.closePath();
+
+    this.update();
+  }
+
+  update() {
+    this.opacity -= 0.001;
+    // this.hue++;
+  }
+}
+
+let pixels = [];
+
+let pixel_size = 5;
+let offset = 2;
+
+let x = canvas.width / 2;
+let y = canvas.height / 2;
+
+function createPixel() {
+  if (Math.random() < 0.5) {
+    if (Math.random() < 0.5) {
+      x += pixel_size + offset;
+    } else {
+      x -= pixel_size + offset;
+    }
+  } else {
+    if (Math.random() < 0.5) {
+      y += pixel_size + offset;
+    } else {
+      y -= pixel_size + offset;
+    }
+  }
+
+  pixels.push(new Pixel(x, y, pixel_size, pixel_size));
+
+  for (let i = 0; i < pixels.length; i++) {
+    pixels[i].draw();
+
+    if (pixels[i].opacity < 0) {
+      pixels.shift();
+    }
+  }
+
+  console.log(pixels.length);
+}
+
+function render() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  createPixel();
+
+}
+
+let interval = setInterval(render, 100);
